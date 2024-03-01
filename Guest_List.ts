23b12b14a527cc19676f5f8be95ade0841da0a2a
ZahraKhan-Github-Account <161851131+ZahraKhan-Github-Account@users.noreply.github.com>
2024-03-01 06:55:20 -0800

    Add files via upload

--- Guest_List.ts
+++ Guest_List.ts
@@ -4,5 +4,20 @@ let Guest_List :string[] = ['Mirha','Sidra','Shagufta'];
 
 for(let i=0; i<Guest_List.length; i++){
   
-    console.log('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
+    console.log('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n')
+
 }
+
+let absent_Guest :string = 'Mirha' ;
+
+let new_Guest :string = 'Zahra';
+
+Guest_List[0] = new_Guest ;
+
+for(let i=0; i<Guest_List.length; i++){
+  
+    console.log('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n')
+
+}
+
+console.log(`Mr. ${absent_Guest} is not coming to the party.`)
