e53fde4ff93cf98bd7ba0e1a1796734715b94cfa
ZahraKhan-Github-Account <161851131+ZahraKhan-Github-Account@users.noreply.github.com>
2024-03-01 06:56:11 -0800

    Add files via upload

---
+++ 14Guest_List.ts
@@ -0,0 +1,8 @@
+
+
+let Guest_List :string[] = ['Mirha','Sidra','Shagufta'];
+
+for(let i=0; i<Guest_List.length; i++){
+  
+    console.log('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
+}
