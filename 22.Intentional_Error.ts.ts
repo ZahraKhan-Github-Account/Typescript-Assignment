25ac39acff7d9820c9cecce652050d75b9656d9f
ZahraKhan-Github-Account <161851131+ZahraKhan-Github-Account@users.noreply.github.com>
2024-03-20 09:41:25 -0700

    Add files via upload

---
+++ 22.Intentional_Error.ts.ts
@@ -0,0 +1,14 @@
+
+//If you have not received an array index error in one of your programs yet, try to make one happen.
+
+// Creating array
+let newArray :string[] = ['Will smith', 'James Bond', 'Tom Cruise'];
+
+// Accessig array by index
+console.log(newArray[5]);
+
+
+//Make sure you correct the error before closing the program.
+
+//Correct the error
+console.log(newArray);
